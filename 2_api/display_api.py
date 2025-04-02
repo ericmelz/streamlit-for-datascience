9a6f78e49c1d1ee4d5e86797bb9837a2ae84cfbd
Eric Melz <eric@emelz.com>
2025-04-02 11:18:12 -0700

    added display_api.py

---
+++ 2_api/display_api.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# display almost anything
+st.write('Hello world')
+st.write('Welcome to Streamlit App APIs')
+st.write(1234)
+
+df = pd.DataFrame({
+    'first_column': [1, 2, 3, 4],
+    'second_column': [10, 20, 30, 40]
+})
+st.write(df)
+
+st.write(np.array([1, 2, 3, 4]))
+
