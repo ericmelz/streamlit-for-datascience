00e6e86e64a84a2d68c557164caf9758972ee3ce
Eric Melz <eric@emelz.com>
2025-04-03 11:19:38 -0700

    tweaks after reading solutions

--- 2_api/assignment1.py
+++ 2_api/assignment1.py
@@ -1,35 +1,26 @@
 import streamlit as st
 
-st.header('Registration Form')
+with st.form('myform'):
+    st.subheader('Registration Form')
 
-col1, col2, col3 = st.columns(3)
-with col1:
-    title = st.selectbox('', ['', 'Mr', 'Mrs', 'Ms'])
+    col1, col2, col3 = st.columns(3)
+    title = col1.selectbox('', ['', 'Mr', 'Mrs', 'Ms'])
+    first_name = col2.text_input('First Name')
+    last_name = col3.text_input('Last Name')
+    designation = st.selectbox('Designation', ['Software', 'Hardware'])
+    dob = st.date_input('Date of Birth', '1960-01-01')
+    gender = st.radio('Gender', ('Male', 'Female', 'Prefer Not to Say'))
+    age = st.slider('Age', 1, 100, 50)
 
-with col2:
-    first_name = st.text_input('First Name')
-
-with col3:
-    last_name = st.text_input('Last Name')
-
-designation = st.selectbox('Designation', ['Software', 'Hardware'])
-
-dob = st.date_input('Date of Birth')
-
-gender = st.radio('Gender', ('Male', 'Female', 'Prefer Not to Say'))
-
-age = st.slider('Age', 1, 100, 50)
-
-submit_button = st.button('Submit')
-
-if submit_button:
-    info = {
-        'title': title,
-        'first_name': first_name,
-        'last_name': last_name,
-        'designation': designation,
-        'dob': dob,
-        'gender': gender,
-        'age': age
-    }
-    st.json(info)
+    submitted = st.form_submit_button('Submit')
+    if submitted:
+        st.success('Form Submitted Successfully')
+        name = f'{title} {first_name} {last_name}'
+        info = {
+            'name': name,
+            'designation': designation,
+            'dob': dob,
+            'gender': gender,
+            'age': age
+        }
+        st.json(info)
